9035744c80d67cb8edca3c78b35686d036d1437d
luzvanessa28 <luzvanessa.ps@gmail.com>
2024-04-21 20:46:01 -0600

    refactor(admin): add a guardian to the admin route

--- src/app/views/admin/admin-routing.module.ts
+++ src/app/views/admin/admin-routing.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddUserComponent } from './user/add-user/add-user.component';
-import { UsersComponent } from './user/users/users.component';
 import { AdminComponent } from './admin.component';
 import { CategoriesComponent } from './category/categories/categories.component';
 import { ProductsComponent } from './product/products/products.component';
 import { AddCategoryComponent } from './category/add-category/add-category.component';
 import { AddProductComponent } from './product/add-product/add-product.component';
 import { DetailCategoryComponent } from './category/detail-category/detail-category.component';
-import { DetailUserComponent } from './user/detail-user/detail-user.component';
 import { DetailProductComponent } from './product/detail-product/detail-product.component';
+import { UsersComponent } from './user/users/users.component';
+import { AddUserComponent } from './user/add-user/add-user.component';
 
 const routes: Routes = [
   {
@@ -33,22 +32,6 @@ const routes: Routes = [
         component: DetailCategoryComponent
       },
       {
-        path: 'users',
-        component: UsersComponent
-      },
-      {
-        path: 'add-user',
-        component: AddUserComponent
-      },
-      {
-        path: 'update-user/:id',
-        component: AddUserComponent
-      },
-      {
-        path: 'detail-user/:id',
-        component: DetailUserComponent
-      },
-      {
         path: 'detail-product/:id',
         component: DetailProductComponent
       },
@@ -63,6 +46,18 @@ const routes: Routes = [
       {
         path: 'update-product/:id',
         component: AddProductComponent
+      },
+      {
+        path: 'users',
+        component: UsersComponent
+      },
+      {
+        path: 'add-user',
+        component: AddUserComponent
+      },
+      {
+        path: 'update-user/:id',
+        component: AddUserComponent
       },
       {
         path: '**',
